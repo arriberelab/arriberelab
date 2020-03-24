ef918c4f6bf258f3dc74878067487d225e262535
marcusvis <marcus.viscardi@gmail.com>
2020-03-24 11:39:46 -0700

    Minor changes + pyx and pickle import change

--- common.py
+++ common.py
@@ -1,10 +1,12 @@
+#!/usr/bin/env python3
 """
 Joshua Arribere Aug 10, 2013
+Converted to python 3: Mar 24, 2020
 
 Several commonly used scripts
 """
-from pyx import *
-import cPickle
+from pyx import color
+from pickle import load, dump
 
 
 def reverseTranslate(seq):
@@ -111,7 +113,7 @@ def writeFasta(dict1, outFile):
     """Given a dict of {name:seq} and an outFile, will write a fasta-formatted file"""
     with open(outFile, 'w') as f:
         for (name, seq) in dict1.iteritems():
-            f.write('>%s\n%s\n' % (name, seq))
+            f.write(f'>{name}\n{seq}\n')
 
 
 def digest(list1, length):
@@ -137,12 +139,12 @@ def partialDigest(list1, length):
 
 def unPickle(file):
     with open(file, 'r') as f:
-        return cPickle.load(f)
+        return load(f)
 
 
 def rePickle(obJect, file):
     with open(file, 'w') as f:
-        cPickle.dump(obJect, f, protocol=2)
+        dump(obJect, f, protocol=2)
 
 
 def parseGeneList(file):
@@ -206,18 +208,18 @@ def colors(i):
          ]
     """
     a=[    
-            color.cmyk(1,0.5,0,0),#blue
-            color.cmyk(0.8,0,0,0),
-            color.cmyk(0.17,0,0.29,0.51),#green
-            color.cmyk(0.17,0,0.33,0.34),
-            color.cmyk(0,0.36,0.71,0),#poppy
-            color.cmyk(0,0.16,0.71,0.02),
-            color.cmyk(0,0.8,1,0),#vermillion
-            color.cmyk(0,0.8,0.8,0.1),
-            color.cmyk(0.1,0.7,0,0),
-            color.cmyk(0.1,0.05,0.9,0),
-            color.cmyk(0.97,0,0.75,0),
-            color.cmyk(0,0,0,1)
+            pyx.color.cmyk(1,0.5,0,0),#blue
+            pyx.color.cmyk(0.8,0,0,0),
+            pyx.color.cmyk(0.17,0,0.29,0.51),#green
+            pyx.color.cmyk(0.17,0,0.33,0.34),
+            pyx.color.cmyk(0,0.36,0.71,0),#poppy
+            pyx.color.cmyk(0,0.16,0.71,0.02),
+            pyx.color.cmyk(0,0.8,1,0),#vermillion
+            pyx.color.cmyk(0,0.8,0.8,0.1),
+            pyx.color.cmyk(0.1,0.7,0,0),
+            pyx.color.cmyk(0.1,0.05,0.9,0),
+            pyx.color.cmyk(0.97,0,0.75,0),
+            pyx.color.cmyk(0,0,0,1)
             ]
     """
     return a[i % len(a)]
