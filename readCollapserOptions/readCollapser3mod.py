b0721ef1766030073b326f8e61570c4031583ea5
Parissa Monem <62483661+pmonem@users.noreply.github.com>
2020-03-30 13:06:06 -0700

    cleaned up

--- readCollapserOptions/readCollapser3mod.py
+++ readCollapserOptions/readCollapser3mod.py
@@ -5,9 +5,7 @@ Converted to python 3: Mar 23, 2020
 Script to collapse reads containing the exact same sequence. 
     Will pick the read information from the first occurence of that read for the outFile.
     
-Mar 24, 2020: 
-    Contains main functions to toggle between for various UMI situations.
-    Make sure to comment out the nonapplicable sections!!
+Mar 30, 2020: Parissa updated to make use of UMI lengths given in command line.
 
 Input: reads.fastq - fastq-formatted reads
 
@@ -19,47 +17,7 @@ run as python readCollapser3.py inFile.fastq number5'N number3'N outPrefix
 import sys, common, collections, numpy
 from logJosh import Tee
 
-"""
 
-############################################################################################################
-#Use the following to trim 6N from the 3'end
-############################################################################################################
-def main(args):
-    inFile,outPrefix=args[0:]
-    print('trimming 6Ns from the 3\'end')
-    reads=collections.defaultdict(lambda:collections.defaultdict(int))
-    with open(inFile,'r') as f:
-        with open(outPrefix,'w') as g:
-            currRead=[]
-            for line in f:
-                line=line.strip()
-                if len(currRead)==4:
-                    #change the bracketed numbers for currReads 1 through 3 below based on the UMI used
-                    UMI=currRead[1][-6:]
-                    if reads[currRead[1][:-6]][UMI]==0:
-                        g.write('%s\n%s\n%s\n%s\n'%(currRead[0].replace(' ','-'),currRead[1][:-6],currRead[2],currRead[3][:-6]))
-                        if len(currRead[1])!=len(currRead[3]):
-                            print(currRead, sys.exit())
-                        #must trim quality scores and reads simultaneously. Hence the double -6
-                    reads[currRead[1][:-6]][UMI]+=1
-                    currRead=[]
-                    currRead.append(line)
-                else:
-                    currRead.append(line)
-
-    #now look at complexity
-    cntr=[]
-    for read in reads:
-        if 2<=sum(reads[read].values())<=5:
-            #cntr.append(float(len(reads[read]))/sum(reads[read].values()))
-            cntr.append(sum([1 for barcode in reads[read] if reads[read][barcode]==1])/float(len(reads[read])))
-    print('%s average +/-SD fraction of reads with 2-5 counts with unique barcodes: % s+/- %s'%(inFile,numpy.average(cntr), numpy.std(cntr)))
-
-"""
-
-############################################################################################################
-#Use the following to trim 6N from the 3'end and 4N from the 5'end
-############################################################################################################
 def main(args):
     inFile,x,y,outPrefix=args[0:]                                       #x=5'N y=3'N
     print('trimming %sNs from the 3\'end and %sNs from 5\'end'%(int(x), int(y)))
@@ -96,45 +54,6 @@ def main(args):
             cntr.append(sum([1 for barcode in reads[read] if reads[read][barcode]==1])/float(len(reads[read])))
     print('%s average +/-SD fraction of reads with 2-5 counts with unique barcodes: % s+/- %s'%(inFile,numpy.average(cntr), numpy.std(cntr)))
 
-"""
-
-############################################################################################################
-#Use the following to trim NOTHING and NOT COLLAPSE READS
-############################################################################################################
-def main(args):
-    inFile,outPrefix=args[0:]
-    print('NOT TRIMMING 6Ns FROM THE 3\'end!!!')
-    print ('NOT COLLAPSING READS!')
-    reads=collections.defaultdict(lambda:collections.defaultdict(int))
-    with open(inFile,'r') as f:
-        with open(outPrefix,'w') as g:
-            currRead=[]
-            for line in f:
-                line=line.strip()
-                if len(currRead)==4:
-                    UMI=currRead[1][-6:]
-                    if True:
-                        g.write('%s\n%s\n%s\n%s\n'%(currRead[0].replace(' ','-'),currRead[1],currRead[2],currRead[3]))
-                        if len(currRead[1])!=len(currRead[3]):
-                            print(currRead, sys.exit())
-                        #must trim quality scores and reads simultaneously. Hence the double -6
-                    reads[currRead[1][:-6]][UMI]+=1
-                    currRead=[]
-                    currRead.append(line)
-                else:
-                    currRead.append(line)
-
-    #now look at complexity
-    cntr=[]
-    for read in reads:
-        if 2<=sum(reads[read].values())<=5:
-            #cntr.append(float(len(reads[read]))/sum(reads[read].values()))
-            cntr.append(sum([1 for barcode in reads[read] if reads[read][barcode]==1])/float(len(reads[read])))
-    print('%s average +/-SD fraction of reads with 2-5 counts with unique barcodes: % s+/- %s'%(inFile,numpy.average(cntr), numpy.std(cntr)))
-
-"""
-
-############################################################################################################
 
 if __name__=='__main__':
     Tee()
