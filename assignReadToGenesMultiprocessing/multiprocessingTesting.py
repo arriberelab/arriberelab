1868849bc4b1972544a1083525d9914c53a05c57
Marcus Viscardi <marcus.visacrdi@gmail.com>
2020-04-01 11:56:56 -0700

    MJV: multiprocessing testing framework

--- assignReadToGenesMultiprocessing/multiprocessingTesting.py
+++ assignReadToGenesMultiprocessing/multiprocessingTesting.py
@@ -12,4 +12,36 @@ Major test will be to compare the speeds of writing a bunch of csv lines to a fi
 Main plan will be to implement a toy method for each of the above then run it at a very high repitition
 """
 
-import
+import pandas as pd
+import numpy as np
+
+# Imports from assignReadsToGenes4.py >>>
+import sys, os, collections, csv, common, re, time, pickle, copy, linecache
+import pandas
+from logJosh import Tee
+csv.field_size_limit(sys.maxsize)
+# <<< Imports from assignReadsToGenes4.py
+
+
+def multiprocessing_with_csv():
+    def single_process_to_csv():
+        pass
+    pass
+
+
+def multiprocessing_with_list():
+    def single_process_to_list():
+        pass
+    pass
+
+
+def vectorized_processing():
+    pass
+
+
+def main(number_of_repititions: int):
+    pass
+
+
+if __name__ == '__main__':
+    main(1)
