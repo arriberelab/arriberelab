eb7a9555d0eadbce8d1e53ff880671561e4baafa
Marcus Viscardi <marcus.visacrdi@gmail.com>
2020-04-01 11:35:53 -0700

    MJV: assignReadsToGenesMultiprocessing

---
+++ assignReadToGenesMultiprocessing/multiprocessingTesting.py
@@ -0,0 +1,13 @@
+"""
+Marcus Viscardi April 1st, 2020
+
+Messing around with multiprocessing for use in assignReadsToGenesX.py script(s)
+
+Major test will be to compare the speeds of writing a bunch of csv lines to a file with:
+    1. Multiprocessing, with each process writing to the csv on its own
+    2. Multiprocessing, with each process writing to a list/dict/df, then that being writen to csv all at once
+    3. Vectorizing (word? not sure), having the process act on the entire df to try and take advantage of the
+        faster C backend of vectorized methods
+
+Main plan will be to impliment a toy method for each of the above then run it at a very high repitition
+"""
