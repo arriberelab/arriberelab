47096182ddb1baf6eba7b24d2783b78b8c4948aa
Marcus Viscardi <marcus.visacrdi@gmail.com>
2020-04-01 22:07:37 -0700

    Noodling?

--- noodling/assignReadToGenesMultiprocessing/multiprocessingTesting.py
+++ noodling/assignReadToGenesMultiprocessing/multiprocessingTesting.py
@@ -11,16 +11,46 @@ Major test will be to compare the speeds of writing a bunch of csv lines to a fi
 
 Main plan will be to implement a toy method for each of the above then run it at a very high repitition
 """
-
+import timeit, csv
+from itertools import repeat
 import pandas as pd
 import numpy as np
-
+"""
 # Imports from assignReadsToGenes4.py >>>
-import sys, os, collections, csv, common, re, time, pickle, copy, linecache
+# import sys, os, collections, csv, common, re, time, pickle, copy, linecache
 # import pandas
-from logJosh import Tee
+#from logJosh import Tee
 csv.field_size_limit(sys.maxsize)
 # <<< Imports from assignReadsToGenes4.py
+"""
+
+
+def timed_csv_write(file, iterations, individual=False):
+    iter_list = range(iterations)
+    iter_dict = {str(n): n for n in iter_list}
+
+    def individual_writes():
+        with open(file, 'a') as f:
+            fwriter = csv.writer(f, delimiter='\t')
+            for _ in iter_dict:
+                fwriter.writerow(_)
+
+    def group_writes():
+        with open(file, 'a') as f:
+            fwriter = csv.writer(f, delimiter='\t')
+            fwriter.writerow((f, iter_dict))
+
+    start = timeit.default_timer()
+    if individual:
+        individual_writes()
+    else:
+        group_writes()
+    end = timeit.default_timer()
+    if individual:
+        print(f'INDIVIDUAL:\n\t{end - start} seconds for {iterations} iterations')
+    else:
+        print(f'GROUP:\n\t{end - start} seconds for {iterations} iterations')
+
 
 
 def multiprocessing_with_csv():
@@ -44,4 +74,6 @@ def main(number_of_repititions: int):
 
 
 if __name__ == '__main__':
-    main(1)
+    iterations = 10**3
+    timed_csv_write('blah.csv', iterations, False)
+    timed_csv_write('blah.csv', iterations, True)
