c493ac53c2d11c9a339440aacfe40421549613a6
Marcus Viscardi <marcus.visacrdi@gmail.com>
2020-04-12 17:12:58 -0700

    More tabbing issues

--- noodling/marcus/otherNoodling/parseSAMToDataframe.py
+++ noodling/marcus/otherNoodling/parseSAMToDataframe.py
@@ -12,7 +12,6 @@ Going to initially run testing with data from:
 
 import sys, os
 import argparse
-import numpy as np
 import pandas as pd
 # Pandas default would cut off basically all the columns so:
 pd.set_option('display.max_rows', 500)
@@ -24,28 +23,28 @@ def parseArgs():
     parser = argparse.ArgumentParser(description="Parse a SAM file into a pandas dataframe")
     parser.add_argument('filename', metavar='filename', type=str, help="Path to .sam file")
     parser.add_argument('headerlines', metavar='headerlines', type=int, help="Number of header lines before data")
-
+    
     args = parser.parse_args()
     file = args.filename
-
+    
     if not os.path.isfile(file):
         print("This is not a file path\n\n\nTERMINATING\n\n")
         sys.exit()
     else:
         print(f"Parsing file @: {file}")
-
+        
     arg_dict = {arg: vars(args)[arg] for arg in vars(args)}
     print(arg_dict)
     return arg_dict
 
 
 def parseSamToDataframe(arg_dict: dict):
-
+    
     o = 'object'
     i = 'int64'
     sam_dtypes_list = [o, i, o, i, i, o, o, i, i, o, o, o, o, o, o]
     sam_dtypes_dict = {i: sam_dtypes_list[i] for i in range(15)}
-
+    
     SAM_df = pd.read_csv(arg_dict['filename'],
                          sep="\t",
                          header=arg_dict['headerlines'],
