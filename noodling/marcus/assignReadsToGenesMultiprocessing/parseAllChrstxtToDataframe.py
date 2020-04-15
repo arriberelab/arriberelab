2f4bfe2e324ae448a71f27c9c826a4847966d910
Marcus Viscardi <marcus.visacrdi@gmail.com>
2020-04-14 19:40:45 -0700

    Split chromosome and print cli-arg functionality

--- noodling/marcus/assignReadsToGenesMultiprocessing/parseAllChrstxtToDataframe.py
+++ noodling/marcus/assignReadsToGenesMultiprocessing/parseAllChrstxtToDataframe.py
@@ -26,10 +26,12 @@ def parseArgs():  # Adapted from parseFastqToDataframe.py
     parser = argparse.ArgumentParser(description="Parse a *.allChrs.txt annotations file into a pandas dataframe")
     parser.add_argument('filename', metavar='filename', type=str,
                         default=None, help="Path to *.allChrs.txt file")
-    parser.add_argument('-s', '--split_chrs', metavar='split_chrs', type=bool,
-                        default=False, help="Option to output several dataframes, one for each chromosome")
+    parser.add_argument('-s', '--split_chrs', action='store_true',
+                        help="Boolean flag to split chromosomes and return a dict of dataframes")
     parser.add_argument('-n', '--num_lines', metavar='num_lines', type=int,
                         default=None, help="Option to only read N number of lines of file")
+    parser.add_argument('-p', '--print_rows', metavar='print_rows', type=int,
+                        default=None, help="Option to print 'n' number of lines of final dataframe")
 
     args = parser.parse_args()
 
@@ -50,25 +52,53 @@ def parseArgs():  # Adapted from parseFastqToDataframe.py
     return arg_dict
 
 
-def parseAllChrsToDataframe(filename, num_lines=None, split_chrs=False):
+def parseAllChrsToDataframe(filename, num_lines=None, split_chrs=False, print_rows=None, deep_memory=None):
+    # Quick check to ensure the passed file path exists
+    if not os.path.isfile(filename):
+        print(f"\nFile does not exist at: {filename}, Terminating Script\n")
+        sys.exit()
+    else:
+        print(f"\nParsing identified file at: {filename}")
+    
     if num_lines:
         annot_df = pd.read_csv(filename,
                                # index_col=0,
                                delimiter='|',
-                               names=['chr_index', 'genes'],
+                               names=['chr', 'genes'],
                                nrows=num_lines,
                                )
     else:
         annot_df = pd.read_csv(filename,
                                # index_col=0,
                                delimiter='|',
-                               names=['chr_index', 'genes'],
+                               names=['chr', 'genes'],
                                )
-    # print(annot_df)
-    annot_df[['chr', 'chr_pos']] = pd.DataFrame(annot_df['chr_index'].str.split('_').values.tolist(),
+    # Split the chr_index column into two
+    annot_df[['chr', 'chr_pos']] = pd.DataFrame(annot_df['chr'].str.split('_').values.tolist(),
                                                 index=annot_df.index)
-    ################
-    print(annot_df)
+    # Sort by Chromosome and index on chromosome
+    annot_df = annot_df.sort_values(by=['chr', 'chr_pos'])
+    # TODO: Reorganize DF columns
+    # TODO: Add a lot of the functionality of the parseSAMToDataframe
+    
+    print(f'Finished parsing and sorting of file at: {filename}\n')
+    
+    if print_rows:
+        print(f"\nFirst {print_rows} rows of dataframe:\n", annot_df.head(print_rows), '\n')
+    if deep_memory:
+        print(annot_df.info(memory_usage='deep'))
+        
+    if split_chrs:
+        # Right now this is the only hardcoded piece which will cause major issues, the column containing the chrs
+        #  has to be explicitly passed into this. Besides that it is extremely reusable
+        annot_df_dict = dict(tuple(annot_df.groupby('chr')))
+        if print_rows:
+            for chr, df in annot_df_dict.items():
+                print(f"\nFirst {print_rows} rows of Chromosome-{chr}:\n", df.head(print_rows))
+        print(f"\nSplit dataframe into {len(annot_df_dict.keys())} separate df's:\n{annot_df_dict.keys()}")
+        return annot_df_dict
+    else:
+        return annot_df
 
 
 if __name__ == '__main__':
