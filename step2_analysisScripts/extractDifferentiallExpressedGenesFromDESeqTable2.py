ae20d0905d934d38be6f908862c193bc0f0f792d
jaarribere <jaarribere@gmail.com>
2020-04-24 10:26:08 -0700

    JAA added FC cutoff to extractDifferentiallExpressedGenesFromDESeqTable2.

--- step2_analysisScripts/extractDifferentiallExpressedGenesFromDESeqTable2.py
+++ step2_analysisScripts/extractDifferentiallExpressedGenesFromDESeqTable2.py
@@ -6,13 +6,15 @@ Script to extract lists of genes deemed to be differentially expressed from
 
 Input: inFile_S/AS.DESeqgeneCts_diffExpression - output from R of nbinomTest, written as a .csv file
    p_val - p-value cutoff. Will pull out everything less than or equal to this p-val (padj)
+   FC - fold change cutoff. Will only pull out genes that change at least this much.
+    If you don't want to deal with this option, just use 1 (one fold change).
 
-run as python extractDifferentiallExpressedGenesFromDESeqTable.py inFile_S.FESeqgeneCts_diffExpression p_val outPrefix
+run as python extractDifferentiallExpressedGenesFromDESeqTable.py inFile_S.FESeqgeneCts_diffExpression p_val FC outPrefix
 """
 import sys
 from logJosh import Tee
 
-def parseInFile(inFile):
+def parseInFile(inFile,FC):
    """Will parse a .csv file, skipping the first line, outputting second col value and last col value a key,value
    pairs in a dict
    EDIT: Will split into genes that are up/down"""
@@ -21,10 +23,13 @@ def parseInFile(inFile):
       f.readline()
       for line in f:
          line=line.strip().split(',')
-         if float(line[5])<1:
+         foldChange=float(line[5])
+         if foldChange<FC:
             aa[line[1].strip('"')]=float(line[-2])
-         else:
+         elif foldChange>FC:
             bb[line[1].strip('"')]=float(line[-2])
+         else:
+            pass
    return aa,bb
 
 def writeTable(pvalDict,pval,outFile):
@@ -35,9 +40,10 @@ def writeTable(pvalDict,pval,outFile):
          f.write(gene+'\n')
 
 def main(args):
-   inFile,pval,outPrefix=args[0:]
+   inFile,pval,FC,outPrefix=args[0:]
    
-   pvalDictUp,pvalDictDown=parseInFile(inFile)
+   FC=float(FC)
+   pvalDictUp,pvalDictDown=parseInFile(inFile,FC)
    
    pval=float(pval)
    writeTable(pvalDictUp,pval,outPrefix+'Up.txt')
