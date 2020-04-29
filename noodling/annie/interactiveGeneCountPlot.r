3b08d5561e49e62cca3be081f540553892253a8f
Annie <acourney@ucsc.edu>
2020-04-28 22:54:01 -0700

    add highlights for genes in interactiveGenePlot.r

--- noodling/annie/interactiveGeneCountPlot.r
+++ noodling/annie/interactiveGeneCountPlot.r
@@ -1,18 +1,30 @@
+#########################
+# Annie Courney 4/28/2020 
+#Script to plot genecounts (output from medianNormalizer), highlighting specific genes in a csv file
+#########################
+
 library("plotly")
 library("tidyverse")
 library("htmlwidgets")
 library("ggplot2")
+library("gghighlight")
+
+#set theme to get gghighlight to work
+theme_set(theme_bw())
 
 #assign csv files to variables
 DESeqGeneCts <- read.csv("C:/Users/aecou/github/arribere_lab/working/smg6RnaSeq/200428_SJA246-248_S.DESeqgeneCts.csv")
 GeneNames <- read.csv("C:/Users/aecou/github/arribere_lab/working/smg6RnaSeq/GeneNames.csv")
 
 #make a list of important genes
-importantGenes[which(DESeqGeneCts[,1] %in% GeneNames),]
+importantGenes <- which(DESeqGeneCts[,1] %in% GeneNames)
 
 #assign the ggplot to variable genePlot
-genePlot <- ggplot(data=DESeqGeneCts, aes(x=X200210_SJA246Nugen, y=X200210_SJA248Nugen, text=paste("Gene: ", GeneNames))) + geom_point(alpha=0.7, colour = "#51A0D5") + labs(x="wt", y="smg-6") + theme_classic() + scale_x_continuous(trans='log10') +
-  scale_y_continuous(trans='log10')
+genePlot <- ggplot(data=DESeqGeneCts, aes(x=X200210_SJA246Nugen, y=X200210_SJA248Nugen)) + geom_point(alpha=0.7, colour = "#51A0D5") + gghighlight(Gene_Name %in% importantGenes,
+                                                                                                                                                   unhighlighted_params = list("darkred", 0.4),
+                                                                                                                                                   use_direct_label = TRUE,
+                                                                                                                                                   label_key = name,
+                                                                                                                                                   label_params = list(size = 5)) + labs(x="wt", y="smg-6") + coord_trans(x="log10", y="log10")
 
 #plot it
 ggplotly(genePlot)
