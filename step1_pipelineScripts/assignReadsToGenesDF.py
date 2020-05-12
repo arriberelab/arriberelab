12ac53859bea5189ff0c738777e6fe784f5a75c3
Marcus Viscardi <marcus.visacrdi@gmail.com>
2020-05-11 19:10:28 -0700

    Trying to identify why unassigned reads made it through

--- step1_pipelineScripts/assignReadsToGenesDF.py
+++ step1_pipelineScripts/assignReadsToGenesDF.py
@@ -551,10 +551,10 @@ def main(sam_file: str, annot_file: str, output_prefix: str,
     if not unassigned_all_chrs.empty:
         unassigned_all_chrs.sort_values(by=['chr', 'chr_pos'], inplace=True)
         unassigned_all_chrs['HI:NH'] = unassigned_all_chrs.apply(lambda row:\
-                                                                 row.HI.split(':')[-1] + row.NH.split(':')[-1])
+                                                                 row['HI'].split(':')[-1] + row['NH'].split(':')[-1])
         unassigned_all_chrs.to_csv(f"{output_prefix}.unassignedReads.jelly",
                                    index=False, sep='\t',
-                                   columns=jam_columns.pop('HI:NH'))
+                                   columns=jam_columns)
     else:
         print(f"\nNo unassigned reads. {output_prefix}.unassignedReads.jelly will not be generated.")
     ####################################################################################################################
